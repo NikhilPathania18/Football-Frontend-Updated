1a7cb40aeb4f8bf7492a5e12baf66efd0ef3706f
Nikhil Pathania <88372381+NikhilPathania18@users.noreply.github.com>
2024-02-17 01:36:57 +0530

    backend endpoint changed

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-export const API = axios.create({baseURL: `http://localhost:8000`});
-// export const API = axios.create({baseURL: `https://football-backend-nikhil.vercel.app`});
+// export const API = axios.create({baseURL: `http://localhost:8000`});
+export const API = axios.create({baseURL: `https://football-backend-nikhil.vercel.app`});
 // export const API = axios.create({baseURL: `http://192.168.0.104:8000`});
 
 // API.interceptors.request.use((req) => {
